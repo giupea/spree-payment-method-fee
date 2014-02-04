c5ea7a6cec4a1ae10da66196f5051978c84eb795
Marco Ventra <marco.ventra@devise.it>
2014-02-04 11:36:39 +0100

    Aggiunte virgolette a [data-hook].

--- app/overrides/payment_method_fees.rb
+++ app/overrides/payment_method_fees.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   virtual_path: "spree/admin/payment_methods/_form",
   name: "payment_method_fees",
-  insert_after: '[data-hook=description]',
+  insert_after: '[data-hook="description"]',
   partial: "spree/admin/payment_methods/fees",
   disabled: false
 )
